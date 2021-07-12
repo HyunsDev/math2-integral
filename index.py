ff0fa163b31b6c19337c695b25021aa9c328f5e3
혀느현스 <hyuns@hyuns.dev>
2021-07-12 21:14:49 +0900

    🎉 1.0.0 완성!

--- index.py
+++ index.py
@@ -1,46 +1,95 @@
 import sympy as sym
+import time
+import decimal
 
-class integral:
-    def __init__(self, expresstion:str, x1, x2) -> None:
+class Integral:
+    def __init__(self, expresstion:str, x1:int, x2:int, debug:bool=False):
+        """
+        계산할 정적분식을 입력합니다.
+        """
+
         expresstion = expresstion.replace("^", "**")
         self.expresstion = expresstion
-        self.x1 = int(x1)
-        self.x2 = int(x2)
+        self.x1 = decimal.Decimal(str(x1))
+        self.x2 = decimal.Decimal(str(x2))
+        self.debug = debug
 
-    def integral_expresstion(self) -> None:
+    def integral_expresstion(self) -> float:
+        "심파이의 integrate를 이용하여 정적분을 계산합니다."
+
         x = sym.Symbol('x')
         a = sym.integrate( eval(self.expresstion) , (x, self.x1, self.x2))
-        sym.pprint(a)
+        return a
         
-    def exp(self, x) -> int,float:
-        return(eval(self.expression))
+    def exp(self, x) -> float:
+        """
+        등록된 식을 계산하여 y값을 리턴합니다. (적분이 아닙니다.)
+        """
+        return decimal.Decimal(str(eval(self.expresstion)))
 
-    def square(self, x1, x2) -> int,float:
+    def square_left(self, x1, x2) -> float:
+        """
+        x1과 x2를 받아 x1의 함수값을 기준으로 사각형의 넓이를 리턴합니다.
+        리만 왼쪽합을 구할 때 사용됩니다.
+        """
+
         y1 = self.exp(x1)
-        y2 = self.exp(x2)
-        y = (y1 + y2) / 2
-        square = (x2 - x1) * y
+        y = y1
+        square = decimal.Decimal(str(x2 - x1)) * y 
         return square
 
-    def square_integral(self, divisor):
+    def square_right(self, x1, x2) -> float:
+        """
+        x1과 x2를 받아 x1의 함수값을 기준으로 사각형의 넓이를 리턴합니다.
+        리만 오른쪽합을 구할 때 사용됩니다.
+        """
+        y2 = self.exp(x2)
+        y = y2
+        square = decimal.Decimal(str(x2 - x1)) * y 
+        return square
+
+    def square_integral(self, divisor:int, mode:str) -> float:
+        """
+        등록된 식을 리만합을 통해 계산합니다.
+        mode를 이용하여 리만 왼쪽합과 오른쪽 합을 선택할 수 있습니다. (left, right를 이용하여 선택)
+        """
+
         length = self.x2 - self.x1
-        unit = length / divisor
-        area = 0 
+        unit = decimal.Decimal(str(length / divisor))
+        area = decimal.Decimal(str(0))
 
-        _x1 = self.x1 - unit
         for i in range(1, divisor):
-            _x1 = _x1 + unit
-            _x2 = _x1 + unit
+            _x1 = decimal.Decimal(str(self.x1 + unit * (i - 1)))
+            _x2 = decimal.Decimal(str(self.x1 + unit * i))
+            if mode == "left":
+                square_area = decimal.Decimal(str(self.square_left(_x1, _x2)))
+            elif mode == "right":
+                square_area = decimal.Decimal(str(self.square_right(_x1, _x2)))
+            area += square_area
 
-            area += self.square(_x1, _x2)
-
-        sym.pprint(area)
+            if self.debug: print(f"[{i}/{divisor} 번째] {square_area}(총 {area})")
+        return area
 
 
 if __name__ == "__main__":
     expresstion = input("수식을 입력하세요: y=")
-    x1 = input("적분의 아래끝을 입력하세요: ")
-    x2 = input("적분의 위끝을 입력하세요: ")
-    a = integral(expresstion, x1, x2)
-    a.integral_expresstion()
-    a.square_integral(10)
+    x1 = int(input("적분의 아래끝을 입력하세요: "))
+    x2 = int(input("적분의 위끝을 입력하세요: "))
+    divisor = int(input("사각형을 얼마나 나눌지 입력하세요: "))
+    mode = input("어떤 리만합을 사용할까요? (left/right): ")
+
+    start = time.time()
+    a = Integral(expresstion, x1, x2, debug=True)
+    a1 = a.integral_expresstion()
+    a2 = a.square_integral(divisor,mode)
+    elapsed_time = round(time.time() - start,3)
+    print("="*20)
+    print(f"수식: y={expresstion}")
+    print(f"범위: {x1}, {x2}")
+    print(f"리만 합: {mode}, {divisor}로 나눔")
+    print("="*20)
+    print(f"리만 합을 이용한 답: {a2}")
+    print(f"정적분을 이용한 값: {a1}")
+    print(f"두 값의 차: {abs(a2-a1)}")
+    print("="*20)
+    print(f"소모 시간: {elapsed_time}초")
